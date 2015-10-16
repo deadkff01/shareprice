78a1cd1433e0e4282e88325b7829d9ab27b9433c
Lucas Gutier <lucasgutier91@gmail.com>
2015-10-15 22:59:11 -0300

    Começo da implementação da pesquisa
    Começo da implementação da pesquisa

---
+++ controller/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+include "../controller/DatabaseConnection.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Shareprice - Pesquisar promoção</title>
+</head>
+<?php
+
+$id_marca = $_POST['id_marca'];
+$id_vestuario = $_POST['id_vestuario'];
+$id_loja = $_POST['id_loja'];
+$preco = $_POST['preco'];
+
+
+?>
+</html>
